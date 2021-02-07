480eb8e091335545406d732e57a2eaa3433c4a23
Kamil Kozioł <koziol.kamil@protonmail.com>
2021-02-07 14:35:24 +0100

    Crossbow

--- src/main/java/com/luckyblocks/listeners/CrossBowListener.java
+++ src/main/java/com/luckyblocks/listeners/CrossBowListener.java
@@ -4,6 +4,7 @@ import com.luckyblocks.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
@@ -13,26 +14,28 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.CrossbowMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CrossBowListener implements Listener {
 
     @EventHandler
     public void onEntityShootBow(EntityShootBowEvent e) {
         if(e.getEntity() instanceof Player) {
             if(e.getBow().getType() == Material.CROSSBOW) {
-                CrossbowMeta meta = (CrossbowMeta) e.getBow().getItemMeta().clone();
-                ItemStack arrow = new ItemStack(Material.ARROW, 1);
-                meta.addChargedProjectile(arrow);
-
-
                 new BukkitRunnable() {
 
                     @Override
                     public void run() {
-                        Bukkit.broadcastMessage("Hello2");
+                        CrossbowMeta meta = (CrossbowMeta) e.getBow().getItemMeta().clone();
+                        ItemStack arrow = new ItemStack(Material.ARROW, 1);
+                        List<ItemStack> projectiles = new ArrayList<ItemStack>();
+                        projectiles.add(arrow);
+                        meta.setChargedProjectiles(projectiles);
                         e.getBow().setItemMeta(meta);
                     }
 
-                }.runTaskLater(Main.getPlugin(Main.class), 10);
+                }.runTaskLater(Main.getPlugin(Main.class), 5);
             }
         }
     }
