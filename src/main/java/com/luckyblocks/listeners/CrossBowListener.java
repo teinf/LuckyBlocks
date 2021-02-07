f320e6a80f1145d8968a202674825e82eb7d6b28
Kamil Kozioł <koziol.kamil@protonmail.com>
2021-02-07 14:05:22 +0100

    Checking

--- src/main/java/com/luckyblocks/listeners/CrossBowListener.java
+++ src/main/java/com/luckyblocks/listeners/CrossBowListener.java
@@ -1,7 +1,10 @@
 package com.luckyblocks.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
@@ -12,8 +15,11 @@ public class CrossBowListener implements Listener {
 
     @EventHandler
     public void onEntityShootBow(EntityShootBowEvent e) {
+        Bukkit.broadcastMessage("Hello");
         if(e.getEntity() instanceof Player) {
+            Bukkit.broadcastMessage("Hello1");
             if(e.getBow().getType() == Material.CROSSBOW) {
+                Bukkit.broadcastMessage("Hello2");
                 CrossbowMeta meta = (CrossbowMeta) e.getBow().getItemMeta();
                 ItemStack arrow = new ItemStack(Material.ARROW, 1);
                 meta.addChargedProjectile(arrow);
