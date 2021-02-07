26b9af40679a1ef22c417f2490395ce9fc6f8303
Jakub Łotysz <jakublotysz@gmail.com>
2021-02-07 13:42:57 +0100

    Blaze-BeesQ (#1)
    * - Added "towerSize" + random
    - Set Blaze Health to 0
    
    * - Fixing Blaze Health

--- src/main/java/com/luckyblocks/events/BlazeTowerSurpriseHandler.java
+++ src/main/java/com/luckyblocks/events/BlazeTowerSurpriseHandler.java
@@ -5,15 +5,22 @@ import org.bukkit.entity.Blaze;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
+import java.util.Random;
+
 public class BlazeTowerSurpriseHandler implements LuckyBlockSurpriseHandler {
     @Override
     public void run(Player player, Block block) {
-        Blaze blaze = (Blaze) player.getWorld().spawnEntity(block.getLocation(), EntityType.BLAZE);
-        blaze.setHealth(2);
+        Random random = new Random();
 
-        for (int i = 0; i < 10; i++) {
+        int towerSize = random.nextInt(4) + 3;
+        int blazeHealth = 0;
+
+        Blaze blaze = (Blaze) player.getWorld().spawnEntity(block.getLocation(), EntityType.BLAZE);
+        blaze.setHealth(blazeHealth);
+
+        for (int i = 0; i < towerSize; i++) {
             Blaze child = (Blaze) player.getWorld().spawnEntity(block.getLocation(), EntityType.BLAZE);
-            child.setHealth(2);
+            child.setHealth(blazeHealth);
             blaze.addPassenger(child);
             blaze = child;
         }
